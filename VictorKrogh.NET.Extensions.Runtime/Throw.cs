f6bb1dd87ff6ae1460307480d8459c867f716217
Victor Krogh Hansen <kingodk@outlook.dk>
2023-04-15 08:10:49 +0200

    Added comment to describe what IfNull does

--- VictorKrogh.NET.Extensions.Runtime/Throw.cs
+++ VictorKrogh.NET.Extensions.Runtime/Throw.cs
@@ -2,6 +2,12 @@
 
 public class Throw
 {
+    /// <summary>
+    /// Throws an exception if <paramref name="obj"/> is null.
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <param name="name"></param>
+    /// <exception cref="ArgumentNullException"></exception>
     public static void IfNull(object obj, string name)
     {
         if (obj == null)
